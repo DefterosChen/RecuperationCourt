814cd655a49c84093b4d91942b21d195c56deeff
DefterosChen <281663448@QQ.COM>
2020-08-18 10:25:54 +0800

    2

--- app/src/main/java/com/kyy/recuperationcourt/common/util/ToastUtil.java
+++ app/src/main/java/com/kyy/recuperationcourt/common/util/ToastUtil.java
@@ -8,26 +8,25 @@ import android.widget.Toast;
 
 import androidx.annotation.StringRes;
 
-import com.kyy.recuperationcourt.MyApplication;
 import com.kyy.recuperationcourt.common.other.wheel.waterfall.base.config.GlobalConfig;
 
 
 public class ToastUtil {
     public static void showTips(String tips) {
-        Toast.makeText(MyApplication.getContextObject(), tips, Toast.LENGTH_SHORT).show();
+        Toast.makeText(GlobalConfig.getAppContext(), tips, Toast.LENGTH_SHORT).show();
     }
 
     public static void showTips(@StringRes int resID) {
-        Toast.makeText(MyApplication.getContextObject(), ResUtil.getStr(resID), Toast.LENGTH_SHORT).show();
+        Toast.makeText(GlobalConfig.getAppContext(), ResUtil.getStr(resID), Toast.LENGTH_SHORT).show();
     }
 
     public static void showMsg(final String msg) {
-        if (MyApplication.getContextObject() != null) {
+        if (GlobalConfig.getAppContext() != null) {
             new Thread() {
                 @Override
                 public void run() {
                     Looper.prepare();
-                    Toast.makeText(MyApplication.getContextObject(), msg, Toast.LENGTH_SHORT).show();
+                    Toast.makeText(GlobalConfig.getAppContext(), msg, Toast.LENGTH_SHORT).show();
                     Looper.loop();
                 }
             }.start();
